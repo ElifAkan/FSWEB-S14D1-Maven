1d12a4b9236a372c56efef6f94b7f1ae77d19cc4
ElifAkan <elif.akan.2002@hotmail.com>
2024-09-24 00:11:11 +0300

    S14D1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import com.workintech.cylinder.Circle;
+import com.workintech.cylinder.Cylinder;
+import com.workintech.pool.Cuboid;
+import com.workintech.pool.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
